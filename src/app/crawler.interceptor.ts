1625f1bf167af9652212a660a803531986c3a4ba
ahsanwaheed-1 <ahsanwaheed100@gmail.com>
2024-11-29 20:23:46 +0500

    Added crawler interceptor to the webcrawler for http

--- src/app/crawler.interceptor.ts
+++ src/app/crawler.interceptor.ts
@@ -1,5 +1,15 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const crawlerInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  console.log('Interceptor triggered:', req);
+
+  // Modify the request as needed
+  const modifiedRequest = req.clone({
+    setHeaders: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer YOUR_TOKEN',
+    },
+  });
+
+  return next(modifiedRequest);
 };
