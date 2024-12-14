e64e5090527b273e339fc16123f240ceda6c4745
ahsanwaheed-1 <112880995+ahsanwaheed-1@users.noreply.github.com>
2024-12-14 14:49:40 +0500

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import {crawlerInterceptor} from "./crawler.interceptor";
-
 import { routes } from './app.routes';
 import {provideHttpClient, withInterceptors} from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([crawlerInterceptor])),
   ],
 };
